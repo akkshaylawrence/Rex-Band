97bac8d526a8807ed2c292f1d3e8da756902c38a
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-14 19:50:40 +0530

    edited app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,13 @@
-(Function() {
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
-        authDomain: "rex-band.firebaseapp.com",
-        databaseURL: "https://rex-band.firebaseio.com",
-        projectId: "rex-band",
-        storageBucket: "rex-band.appspot.com"
-    };
-    firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
+    authDomain: "rex-band.firebaseapp.com",
+    databaseURL: "https://rex-band.firebaseio.com",
+    projectId: "rex-band",
+    storageBucket: "rex-band.appspot.com"
+};
+firebase.initializeApp(config);
 
-    var ref = firebase.database().ref('players');
+var ref = firebase.database().ref('players');
 
-    console.log(ref);
-
-
-}());
+console.log(ref);
