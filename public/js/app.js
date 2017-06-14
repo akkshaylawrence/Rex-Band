c40d21ebdf1100a45b56baa8d24edf0a18059ac2
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-14 19:39:33 +0530

    Added app.js file to cleanup code.

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+(Function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
+        authDomain: "rex-band.firebaseapp.com",
+        databaseURL: "https://rex-band.firebaseio.com",
+        projectId: "rex-band",
+        storageBucket: "rex-band.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+    var ref = firebase.database().ref('players');
+
+    console.log(ref);
+
+
+}());
