3b19f24f0e50b4b95656e6fe7f3dde39ab1b01bd
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-20 00:23:42 +0530

    Added Content in Music Page

--- public/js/init.js
+++ public/js/init.js
@@ -6,4 +6,15 @@
 
     $('.parallax').parallax();
 
+    $('input.autocomplete').autocomplete({
+        data: {
+            "One": null,
+            "Two": null,
+            "Three": null
+        },
+        limit: 5,
+        onAutocomplete: function (val) {},
+        minLength: 1
+    });
+
 })(jQuery);
