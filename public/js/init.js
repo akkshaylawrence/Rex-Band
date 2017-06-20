f5161733755309f3940c9b3ffbbdb067496d7f12
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-20 17:28:47 +0530

    Completed Feature Implementation.
    Completed search functionality.for Music Page and also corrected
    parallax on the pages.

--- public/js/init.js
+++ public/js/init.js
@@ -28,3 +28,24 @@ function filterTable() {
         }
     }
 }
+
+$('#autocomplete-input').keyup(function () {
+    var filter_array = new Array();
+    var filter = this.value.toLowerCase();
+    filter_array = filter.split(' ');
+    var arrayLength = filter_array.length;
+    $('.alb').each(function () {
+        var _this = $(this);
+        var title = _this.find('span').text().toLowerCase();
+        var hidden = 0;
+        for (var i = 0; i < arrayLength; i++) {
+            if (title.indexOf(filter_array[i]) < 0) {
+                _this.hide();
+                hidden = 1;
+            }
+        }
+        if (hidden == 0) {
+            _this.show();
+        }
+    });
+});
