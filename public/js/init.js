46095bdbadf5557fb0817881a784a26d62890a4f
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-20 19:38:56 +0530

    Added Firebase.

--- public/js/init.js
+++ public/js/init.js
@@ -1,5 +1,14 @@
 (function ($) {
 
+    var config = {
+        apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
+        authDomain: "rex-band.firebaseapp.com",
+        databaseURL: "https://rex-band.firebaseio.com",
+        projectId: "rex-band",
+        storageBucket: "rex-band.appspot.com",
+    };
+    firebase.initializeApp(config);
+
     $(document).ready(function () {
         $(".button-collapse").sideNav();
     });
