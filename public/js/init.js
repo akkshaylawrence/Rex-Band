e758e4d906e7e089365bc65d077a4f52b976acee
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-20 23:41:16 +0530

    General Updates.

--- public/js/init.js
+++ public/js/init.js
@@ -1,5 +1,4 @@
 (function ($) {
-
     var config = {
         apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
         authDomain: "rex-band.firebaseapp.com",
@@ -8,24 +7,23 @@
         storageBucket: "rex-band.appspot.com",
     };
     firebase.initializeApp(config);
-
     $(document).ready(function () {
-        $(".button-collapse").sideNav();
+        $('.button-collapse').sideNav({
+            menuWidth: 200,
+            edge: 'left',
+            closeOnClick: true,
+            draggable: true
+            }
+        );
     });
-
     $('.parallax').parallax();
-
 })(jQuery);
-
 function filterTable() {
-    // Declare variables 
     var input, filter, table, tr, td, i;
     input = document.getElementById("autocomplete-input1");
     filter = input.value.toUpperCase();
     table = document.getElementById("sTable");
     tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
@@ -37,7 +35,6 @@ function filterTable() {
         }
     }
 }
-
 $('#autocomplete-input').keyup(function () {
     var filter_array = new Array();
     var filter = this.value.toLowerCase();
