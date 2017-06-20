62c54553a664118defcfa074ccb49bb31fdfbb43
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-20 14:55:30 +0530

    Updated Favicon and added search functionality for longview

--- public/js/init.js
+++ public/js/init.js
@@ -6,15 +6,25 @@
 
     $('.parallax').parallax();
 
-    $('input.autocomplete').autocomplete({
-        data: {
-            "One": null,
-            "Two": null,
-            "Three": null
-        },
-        limit: 5,
-        onAutocomplete: function (val) {},
-        minLength: 1
-    });
-
 })(jQuery);
+
+function filterTable() {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("autocomplete-input1");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("sTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
