9b8b98d9482a55c3b487eeacf2e55c72e3f3a33a
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-21 13:04:39 +0530

    Removed firebase temporarily.

--- public/js/init.js
+++ public/js/init.js
@@ -1,12 +1,4 @@
 (function ($) {
-    var config = {
-        apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
-        authDomain: "rex-band.firebaseapp.com",
-        databaseURL: "https://rex-band.firebaseio.com",
-        projectId: "rex-band",
-        storageBucket: "rex-band.appspot.com",
-    };
-    firebase.initializeApp(config);
     $(document).ready(function () {
         $('.button-collapse').sideNav({
             menuWidth: 200,
