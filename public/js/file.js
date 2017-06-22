532dff255f0aba3591799ee06795c20f3426bb15
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-22 20:09:25 +0530

    Completed Download Functionality.

--- public/js/file.js
+++ public/js/file.js
@@ -3,12 +3,27 @@ function fileDl(name) {
     var storageRef = storage.ref("Lyrics/" + name + ".pdf");
     storageRef.getDownloadURL().then(function (url) {
         console.log(url);
+        var filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
+        console.log(filename);
         var xhr = new XMLHttpRequest();
         xhr.responseType = 'blob';
-        xhr.onload = function (event) {
-            var blob = xhr.response;
+        xhr.onload = function () {
+            var a = document.createElement('a');
+            a.href = window.URL.createObjectURL(xhr.response); // xhr.response is a blob
+            a.download = filename; // Set the file name.
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.click();
+            delete a;
         };
         xhr.open('GET', url);
         xhr.send();
+    }).catch(function (error) {
+        switch (error.code) {
+            case 'storage/object_not_found':
+                break;
+            case 'storage/unauthorized':
+                break;
+        }
     });
 }
