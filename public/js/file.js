52aded5f61624b234780ff9a1c81bbe7ed2ec16c
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-22 17:28:54 +0530

    Added missing songs and updated names.

--- public/js/file.js
+++ public/js/file.js
@@ -1,12 +1,19 @@
 // Create a reference to the file we want to download
-var starsRef = storageRef.child('images/stars.jpg');
+var starsRef = storageRef.child('images/');
 // Get the download URL
-starsRef.getDownloadURL().then(function(url) {
-  // Insert url into an <img> tag to "download"
-}).catch(function(error) {
-  switch (error.code) {
-    case 'storage/object_not_found':
-      // File doesn't exist
-      break;
-  }
+var a = document.getElementById('yourlinkId'); //or grab it by tagname etc
+starsRef.getDownloadURL().then(function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function (event) {
+        var blob = xhr.response;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+}).catch(function (error) {
+    switch (error.code) {
+        case 'storage/object_not_found':
+            alert("FILE UNAVAILABLE!!");
+            break;
+    }
 });
