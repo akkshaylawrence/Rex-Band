821dac0e2f808184cb2dc0d32828a03a49d97d68
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-23 12:29:45 +0530

    Updated download code and beautified file name

--- public/js/init.js
+++ public/js/init.js
@@ -1,10 +1,10 @@
 (function ($) {
     var config = {
-    apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
-    authDomain: "rex-band.firebaseapp.com",
-    databaseURL: "https://rex-band.firebaseio.com",
-    projectId: "rex-band",
-    storageBucket: "rex-band.appspot.com",
+        apiKey: "AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",
+        authDomain: "rex-band.firebaseapp.com",
+        databaseURL: "https://rex-band.firebaseio.com",
+        projectId: "rex-band",
+        storageBucket: "rex-band.appspot.com",
     };
     firebase.initializeApp(config);
     $(document).ready(function () {
