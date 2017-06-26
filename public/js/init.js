7292a117bcfe119780bb9f16e51a05394a781244
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-26 13:02:54 +0530

    Changes.
    1. Separated Firebase code.
    2. Removed material icons.
    3. Small bug fixes.

--- public/js/init.js
+++ public/js/init.js
@@ -1,1 +1,57 @@
-function filterTable(){var e,t,a,l,n,o;for(e=document.getElementById("autocomplete-input1"),t=e.value.toUpperCase(),a=document.getElementById("sTable"),l=a.getElementsByTagName("tr"),o=0;o<l.length;o++)n=l[o].getElementsByTagName("td")[0],n&&(n.innerHTML.toUpperCase().indexOf(t)>-1?l[o].style.display="":l[o].style.display="none")}!function(e){var t={apiKey:"AIzaSyDmEcN3NqLcI3mMliJPUM-HUn3rSwyvVFs",authDomain:"rex-band.firebaseapp.com",databaseURL:"https://rex-band.firebaseio.com",projectId:"rex-band",storageBucket:"rex-band.appspot.com"};firebase.initializeApp(t),e(document).ready(function(){e(".button-collapse").sideNav({menuWidth:200,edge:"left",closeOnClick:!0,draggable:!0}),e(".modal").modal(),e(".parallax").parallax(),e("select").material_select(),e(".scrollspy").scrollSpy({scrollOffset:60}),e("table#sTable tr").click(function(){var t=e(this).find("td:first").text();download(t,"Lyrics/")})})}(jQuery),$("#autocomplete-input").keyup(function(){var e=new Array,t=this.value.toLowerCase();e=t.split(" ");var a=e.length;$(".alb").each(function(){for(var t=$(this),l=t.find("span").text().toLowerCase(),n=0,o=0;a>o;o++)l.indexOf(e[o])<0&&(t.hide(),n=1);0==n&&t.show()})});
+(function ($) {
+    $(document).ready(function () {
+        $('.button-collapse').sideNav({
+            menuWidth: 200,
+            edge: 'left',
+            closeOnClick: true,
+            draggable: true
+        });
+        $('.modal').modal();
+        $('.parallax').parallax();
+        $('select').material_select();
+        $('.scrollspy').scrollSpy({
+            scrollOffset: 60
+        });
+        $('table#sTable tr').click(function () {
+            var val = $(this).find('td:first').text();
+            download(val,'Lyrics/');
+        });
+    });
+})(jQuery);
+function filterTable() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("autocomplete-input1");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("sTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+$('#autocomplete-input').keyup(function () {
+    var filter_array = new Array();
+    var filter = this.value.toLowerCase();
+    filter_array = filter.split(' ');
+    var arrayLength = filter_array.length;
+    $('.alb').each(function () {
+        var _this = $(this);
+        var title = _this.find('span').text().toLowerCase();
+        var hidden = 0;
+        for (var i = 0; i < arrayLength; i++) {
+            if (title.indexOf(filter_array[i]) < 0) {
+                _this.hide();
+                hidden = 1;
+            }
+        }
+        if (hidden == 0) {
+            _this.show();
+        }
+    });
+});
