ffc53be07d7286ed169fd968dedfe1570d7c3705
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-27 11:54:59 +0530

    Added Tooltip for subscribe button.

--- public/js/init.js
+++ public/js/init.js
@@ -12,6 +12,7 @@
         $('.scrollspy').scrollSpy({
             scrollOffset: 60
         });
+        $('.tooltipped').tooltip({delay: 20});
         $('table#sTable tr').click(function () {
             var val = $(this).find('td:first').text();
             download(val,'Lyrics/');
