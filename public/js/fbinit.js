ab5f7a32e455ddb35cf8c999cd5529dd430b9c62
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-06-29 15:20:50 +0530

    Vastly Improved Security for database Access.

--- public/js/fbinit.js
+++ public/js/fbinit.js
@@ -8,4 +8,5 @@
     };
     firebase.initializeApp(config);
 })(jQuery);
+firebase.auth().signInAnonymously();
 var database = firebase.database();
--- public/js/form.js
+++ public/js/form.js
@@ -1,12 +1,3 @@
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-}
 function saveForm() {
     var name = document.getElementById('names').value;
     var email = document.getElementById('emails').value;
@@ -15,20 +6,24 @@ function saveForm() {
     var phone = document.getElementById('phones').value;
     var subs = document.getElementById('subs');
     var subbtn = document.getElementById('subbtn');
-    userId = guid();
-    database.ref('Subscribers/' + userId).set({
-        Name: name,
-        Email: email,
-        Country: country,
-        State: state,
-        Phone: phone
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var uid = user.uid;
+            database.ref('Subscribers/' + uid).set({
+                Name: name,
+                Email: email,
+                Country: country,
+                State: state,
+                Phone: phone
+            });
+            subs.classList.add("disabled");
+            subs.innerHTML = "Submitted";
+            Materialize.toast('We will keep in touch!', 4000);
+            subbtn.classList.remove('red')
+            subbtn.innerHTML = "Subscribed";
+            $('#modal1').modal('close');
+        }
     });
-    subs.classList.add("disabled");
-    subs.innerHTML = "Submitted";
-    Materialize.toast('We will keep in touch!', 4000);
-    subbtn.classList.remove('red')
-    subbtn.innerHTML = "Subscribed";
-    $('#modal1').modal('close');
 }
 function saveContact() {
     var firstname = document.getElementById('first_name').value;
@@ -37,15 +32,19 @@ function saveContact() {
     var country = document.getElementById('country').value;
     var text = document.getElementById('textarea1').value;
     var submit = document.getElementById('submit');
-    userId = guid();
-    database.ref('Feedback/' + userId).set({
-        Firstname: firstname,
-        Lastname: lastname,
-        Email: email,
-        Country: country,
-        Message: text
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var uid = user.uid;
+            database.ref('Feedback/' + uid).set({
+                Firstname: firstname,
+                Lastname: lastname,
+                Email: email,
+                Country: country,
+                Message: text
+            });
+            submit.classList.add("disabled");
+            submit.innerHTML = "Submitted";
+            Materialize.toast('We will get back to you soon!', 4000);
+        }
     });
-    submit.classList.add("disabled");
-    submit.innerHTML = "Submitted";
-    Materialize.toast('We will get back to you soon!', 4000);
 }
