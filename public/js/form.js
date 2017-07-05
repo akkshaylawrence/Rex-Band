159753e2efdcf03e565f3a3cb871fabb0460555f
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-05 16:53:52 +0530

    Added validation to forms
    Now you cannot submit a form without entering atlas the email.

--- public/js/form.js
+++ public/js/form.js
@@ -6,24 +6,29 @@ function saveForm() {
     var phone = document.getElementById('phones').value;
     var subs = document.getElementById('subs');
     var subbtn = document.getElementById('subbtn');
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var uid = user.uid;
-            database.ref('Subscribers/' + uid).set({
-                Name: name,
-                Email: email,
-                Country: country,
-                State: state,
-                Phone: phone
-            });
-            subs.classList.add("disabled");
-            subs.innerHTML = "Submitted";
-            Materialize.toast('We will keep in touch!', 4000);
-            subbtn.classList.remove('red')
-            subbtn.innerHTML = "Subscribed";
-            $('#modal1').modal('close');
-        }
-    });
+    if (email == "") {
+        document.getElementById('emails').focus();
+        document.getElementById('emails').classList.add('invalid');
+    } else {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                var uid = user.uid;
+                database.ref('Subscribers/' + uid).set({
+                    Name: name,
+                    Email: email,
+                    Country: country,
+                    State: state,
+                    Phone: phone
+                });
+                subs.classList.add("disabled");
+                subs.innerHTML = "Submitted";
+                Materialize.toast('We will keep in touch!', 4000);
+                subbtn.classList.remove('red')
+                subbtn.innerHTML = "Subscribed";
+                $('#modal1').modal('close');
+            }
+        });
+    }
 }
 function saveContact() {
     var firstname = document.getElementById('first_name').value;
@@ -32,19 +37,31 @@ function saveContact() {
     var country = document.getElementById('country').value;
     var text = document.getElementById('textarea1').value;
     var submit = document.getElementById('submit');
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var uid = user.uid;
-            database.ref('Feedback/' + uid).set({
-                Firstname: firstname,
-                Lastname: lastname,
-                Email: email,
-                Country: country,
-                Message: text
-            });
-            submit.classList.add("disabled");
-            submit.innerHTML = "Submitted";
-            Materialize.toast('We will get back to you soon!', 4000);
+    if (email === "" || text.length < 1) {
+        if (email==="" || email.length < 11) {
+            document.getElementById('email').focus();
+            document.getElementById('email').classList.add('invalid');
         }
-    });
+        if (text.length < 1) {
+            document.getElementById('textarea1').focus();
+            document.getElementById('textarea1').classList.add('invalid');
+        }
+    } else {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                var uid = user.uid;
+                database.ref('Feedback/' + uid).set({
+                    Firstname: firstname,
+                    Lastname: lastname,
+                    Email: email,
+                    Country: country,
+                    Message: text
+                });
+                submit.classList.add("disabled");
+                submit.innerHTML = "Submitted";
+                document.getElementById("clear").reset();
+                Materialize.toast('We will get back to you soon!', 4000);
+            }
+        });
+    }
 }
