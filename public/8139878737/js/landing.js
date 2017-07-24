0c0611db87ccfa6409a6a807ac7a5891efbe0b5e
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-24 12:52:14 +0530

    Bug Fixes.

--- public/8139878737/js/landing.js
+++ public/8139878737/js/landing.js
@@ -4,7 +4,7 @@ document.getElementById("file3").addEventListener('click', addLand, false);
 // document.getElementById("file1").addEventListener('click', addLand, false);
 function addLand() {
 	i += 1;
-	if (i == 1) { //TODO - Change to 3 after comlpeting implementation.
+	if (i == 3) { //TODO - Change to 3 after comlpeting implementation.
 		document.getElementById("trbtn").classList.remove("disabled");
 		document.getElementById("trbtn").style.display = "block";
 	}
@@ -19,25 +19,24 @@ landbtn.addEventListener('click', function () {
 			image1_link: url // adding download url to db Home_Page/image_link : "https:/www.."
 		})
 	})
-	//TODO - Uncomment after comlpeting implementation.
-	//Image2
-	// var file = document.getElementById('file2').files[0];
-	// filename2 = file.name;
-	// var image2 = firebase.storage().ref('Landing_Page/image2'); // Create a reference to uploaded file
-	// image2.put(file).then(function (snapshot) {
-	//    var url = snapshot.downloadURL;
-	//    database.ref('Landing_Page_Images/').update({
-	//       image2_link: url // adding download url to db Home_Page/image_link : "https:/www.."
-	//    })
-	// })
-	// //Image3
-	// var file = document.getElementById('file3').files[0];
-	// filename3 = file.name;
-	// var image3 = firebase.storage().ref('Landing_Page/image3'); // Create a reference to uploaded file
-	// image3.put(file).then(function (snapshot) {
-	//    var url = snapshot.downloadURL;
-	//    database.ref('Landing_Page_Images').update({
-	//       image3_link: url // adding download url to db Home_Page/image_link : "https:/www.."
-	//    })
-	// })
+	Image2
+	var file = document.getElementById('file2').files[0];
+	filename2 = file.name;
+	var image2 = firebase.storage().ref('Landing_Page/image2'); // Create a reference to uploaded file
+	image2.put(file).then(function (snapshot) {
+	   var url = snapshot.downloadURL;
+	   database.ref('Landing_Page_Images/').update({
+	      image2_link: url // adding download url to db Home_Page/image_link : "https:/www.."
+	   })
+	})
+	//Image3
+	var file = document.getElementById('file3').files[0];
+	filename3 = file.name;
+	var image3 = firebase.storage().ref('Landing_Page/image3'); // Create a reference to uploaded file
+	image3.put(file).then(function (snapshot) {
+	   var url = snapshot.downloadURL;
+	   database.ref('Landing_Page_Images').update({
+	      image3_link: url // adding download url to db Home_Page/image_link : "https:/www.."
+	   })
+	})
 });
