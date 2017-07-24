55a5bd3f8a59eda6d0fcd69a8cafb814ef6c1e1e
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-24 12:59:43 +0530

    more Bug Fixes.

--- public/8139878737/js/landing.js
+++ public/8139878737/js/landing.js
@@ -1,7 +1,7 @@
 var i = 0;
 document.getElementById("file3").addEventListener('click', addLand, false);
-// document.getElementById("file2").addEventListener('click', addLand, false);
-// document.getElementById("file1").addEventListener('click', addLand, false);
+document.getElementById("file2").addEventListener('click', addLand, false);
+document.getElementById("file1").addEventListener('click', addLand, false);
 function addLand() {
 	i += 1;
 	if (i == 3) { //TODO - Change to 3 after comlpeting implementation.
